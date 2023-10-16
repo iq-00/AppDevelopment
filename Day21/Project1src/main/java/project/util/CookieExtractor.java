d6ac1ada385c90a7dd80dc5828e6e646ab836327
sub_has <102864350+iq-00@users.noreply.github.com>
2023-10-16 16:01:48 +0530

    Add files via upload

---
+++ Day21/Project1src/main/java/project/util/CookieExtractor.java
@@ -0,0 +1,18 @@
+package project.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieExtractor {
+	public String extractCookie(String cookie, String cName) {
+		String c = null;
+		String cookies[] = cookie.split(";");
+		for (var a : cookies) {
+			if (a.startsWith(!cName.equals(null) ? cName : "user-auth-key")) {
+				c = a.split("=")[1];
+				break;
+			}
+		}
+		return c;
+	}
+}
